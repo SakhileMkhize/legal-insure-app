b559b62747b9ab34a9e1bbe1b5446cdaec3b5710
SakhileMkhize <Sakhile.Mkhize@sanlam.co.za>
2026-07-17 09:16:40 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isProd ? '/legal-insure-app/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
